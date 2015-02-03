13d216067ba60c813721f0b810ddf427aa49c7ef
Matias Saguir <mativs@gmail.com>
2015-02-02 22:19:44 -0300

    Clean unused files

--- deploy/fab/memcached.py
+++
@@ -1,8 +0,0 @@
-from cuisine import package_ensure
-from cuisine import file_update, file_exists, file_unlink
-from cuisine import text_template
-from cuisine import mode_sudo, run, cd
-
-def memcached_ensure():
-    with mode_sudo():
-        package_ensure('memcached') 
