1c0fe7270fb34fcb0bb2498a2d31fa93059e486e
Matias Saguir <mativs@gmail.com>
2015-02-03 15:43:00 -0300

    Heroku deploy

--- server/conf/default.py
+++
@@ -1,5 +0,0 @@
-from server.conf.django import *
-from server.conf.database import *
-from server.conf.translation import *
-from server.conf.static import *
-from server.conf.rest import *
--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,114 @@
-try:
-    from server.conf.local import *
-except ImportError:
-    from server.conf.default import *
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+SITE_ID = 1
+
+INTERNAL_IPS = ['localhost']
+
+SECRET_KEY = '9)l&&h+co!mbul+l*8-2u2)afy!izaxhq#i3wpssflvv#amrrl'
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
+
+ROOT_URLCONF = 'server.urls'
+
+WSGI_APPLICATION = 'server.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'rest_framework',
+    'rest_framework.authtoken',
+)
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+DJANGO_ROOT = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'client'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(DJANGO_ROOT, 'templates'),
+)
+
+from mongoengine import connect
+connect('pipfix')
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
