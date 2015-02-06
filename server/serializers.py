610d63ba88f4fa9fedef1fd086664bfa77724343
Matias Saguir <mativs@gmail.com>
2015-02-06 17:11:43 -0300

    Revert "Add user model"
    This reverts commit 9aca157c1cc167bc44633d92afe17ececc6a0744.

--- server/serializers.py
+++ server/serializers.py
@@ -1,14 +1,6 @@
-from server.documents import Vote, User
+from server.documents import Vote
 from rest_framework_mongoengine.serializers import DocumentSerializer
-from rest_framework.fields import EmailField
 
 class VoteSerializer(DocumentSerializer):
   class Meta:
     model = Vote
-
-class UserSerializer(DocumentSerializer):
-  email=EmailField(required=False)
-
-  class Meta:
-    model = User
-    fields = ('username', 'email', 'twitter_id', "followers")
--- server/views.py
+++ server/views.py
@@ -1,13 +1,9 @@
 from rest_framework_mongoengine.viewsets import ModelViewSet
-from server.documents import Vote, User
-from server.serializers import VoteSerializer, UserSerializer
+from server.documents import Vote
+from server.serializers import VoteSerializer
+
 
 class VoteViewSet(ModelViewSet):
   queryset = Vote.objects.all()
   serializer_class = VoteSerializer
-  lookup_field = "stuff_id"
-
-class UserViewSet(ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
   lookup_field = "stuff_id"
