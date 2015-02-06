40b3b5e6056826f618505acd11bad78f885ebc59
Matias Saguir <mativs@gmail.com>
2015-02-06 17:45:04 -0300

    Now yes, add user model

--- server/serializers.py
+++ server/serializers.py
@@ -1,6 +1,12 @@
-from server.documents import Vote
+from server.documents import Vote, User
 from rest_framework_mongoengine.serializers import DocumentSerializer
+from rest_framework.fields import EmailField
 
 class VoteSerializer(DocumentSerializer):
   class Meta:
     model = Vote
+
+class UserSerializer(DocumentSerializer):
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'twitter_id', "followers")
